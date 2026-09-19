5d0f40707bfd3e57bbe2fcdf2a7bc63b00676c17
sepehrsylvanus <mr.forte.world@gmail.com>
2026-09-20 00:15:11 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Welcome from "./components/pages/Welcome.vue";
+</script>
+
+<template>
+  <div>
+    <h1 class="text-gradient">Hello World!</h1>
+  </div>
+  <Welcome />
+</template>
+
+<style scoped></style>
---
+++ src/components/pages/Welcome.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <section id="welcome">
+    <div class="benefits">
+      <h2>Complete this program<br />if you want...</h2>
+      <div>
+        <p>✅ Follow a simple program with proven results.</p>
+        <p>✅ Get fit, healthy, strong and absoltely shredded.</p>
+        <p>✅ Learn more about gym, training and technique</p>
+      </div>
+    </div>
+    <div>
+      <h3>The Training Plan</h3>
+      <p>
+        This training plan follow a structure known as the
+        <strong>Bro Split</strong>.
+      </p>
+      <h5><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i></h5>
+    </div>
+    <div class="card"></div>
+  </section>
+</template>
+
+<style scoped></style>
